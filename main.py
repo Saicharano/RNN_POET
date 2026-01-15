d8b41e463b94ea0659937d0a5acc9d9813f129fe
Saicharano <bollenasaicharan@gmail.com>
2026-01-15 12:33:23 +0530

    Implement website content fetching and summarization
    Fetch website content and summarize it humorously using ChatGroq.

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+
+import requests
+from bs4 import BeautifulSoup
+from langchain_groq import ChatGroq
+
 def main():
-    print("Hello from rnn-poet!")
+    respond = requests.get("https://www.tensorflow.org/")
+    fetch_website_content = BeautifulSoup(respond.text, 'html.parser')
+    system_prompt="""
+    You are a snarky assisstant that analyzes contents of websites,
+    and provide a short , snarky , humorous summary , ignoring text
+    that might be navigation related. Respond in markdown . Do not wrap the markdown in a code block - respond just with the markdown"""
+    user_prompt="""
+    Here are the contents of a website.
+    Provide a short summary of this website.
+    If it includes news or announcements, then summarize these too.
+    """
+    messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt + fetch_website_content.get_text()}
+    ]
+    response = ChatGroq(model="llama-3.3-70b-versatile",temperature=0)
+    print("Response from Groq LLM: \n", response.choices[0].message.content)
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
